2dc5d856e7fc6a9b7ba83fbc431bbef0d6334c2a
Shinya Kato <s-kato@vish.co.jp>
2020-08-07 18:47:11 +0900

    commit

--- aspnet-core/src/MyCompany.Web.Core/Authentication/External/IExternalAuthConfiguration.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace MyCompany.Authentication.External
-{
-    public interface IExternalAuthConfiguration
-    {
-        List<ExternalLoginProviderInfo> Providers { get; }
-    }
-}
